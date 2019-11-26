567d3cb311af7c71194bb7cd59de2410e8202a39
Dylan Pacheco <dylanpachecocas@gmail.com>
2019-11-26 17:16:45 -0300

    Implementado menu de contexto e refatorado

--- app/src/main/java/com/fatec/banannap2/ui/activity/ListaClientesActivity.kt
+++ app/src/main/java/com/fatec/banannap2/ui/activity/ListaClientesActivity.kt
@@ -3,7 +3,12 @@ package com.fatec.banannap2.ui.activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ContextMenu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.fatec.banannap2.R
 import com.fatec.banannap2.ui.adapter.ListClientesAdapter
 import com.fatec.banannap2.model.Cliente
@@ -17,30 +22,13 @@ class ListaClientesActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cliente)
 
-        val toolbarid = toolbarid2
-        setSupportActionBar(toolbarid)
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-        colappsingtoolbar.title = "Clientes"
+        configuraToolbar()
+        val listaClientes: MutableList<Cliente> = configuraClientesExemplo()
+        configuraRecyclerView(listaClientes)
+        configuraFab()
+    }
 
-        val listaClientes : MutableList<Cliente> = mutableListOf()
-
-        val mutableListOf = mutableListOf<Cliente>(
-            Cliente("Quitanta da Marcia", "Rua x", "Josefina", "9999999"),
-            Cliente("Quitanta da Maria", "Rua y", "Maria", "8888888"),
-            Cliente("Mercado da esquina", "Rua z", "José", "777777"),
-            Cliente("Padaria", "Rua w", "Marcos", "666666"),
-            Cliente("Sacolão do Marcello", "Rua a", "Cara simpática", "5555555")
-            )
-
-        listaClientes.addAll(mutableListOf)
-
-        val recyclerView = list_clientes_recyclerview
-        recyclerView.adapter = ListClientesAdapter(listaClientes, this, onClick = {
-            val intent = Intent(this, DadosClienteActivity::class.java)
-            intent.putExtra("cliente", it)
-            startActivity(intent)
-        })
-
+    private fun configuraFab() {
         val fab = floatingActionButton
         fab.setOnClickListener(View.OnClickListener {
             val vaiParaCadastraCliente = Intent(this, CadastraClienteActivity::class.java)
@@ -48,8 +36,66 @@ class ListaClientesActivity : AppCompatActivity() {
         })
     }
 
+    private fun configuraRecyclerView(listaClientes: MutableList<Cliente>) {
+        //val recyclerView = list_clientes_recyclerview
+
+        val adapter = ListClientesAdapter(listaClientes, this, onClick = {
+            val intent = Intent(this, DadosClienteActivity::class.java)
+            intent.putExtra("cliente", it)
+            startActivity(intent)
+        })
+        list_clientes_recyclerview.adapter = adapter
+        registerForContextMenu(list_clientes_recyclerview)
+    }
+
+    private fun configuraClientesExemplo(): MutableList<Cliente> {
+        val listaClientes: MutableList<Cliente> = mutableListOf()
+
+        val mutableListOf = mutableListOf<Cliente>(
+            Cliente("Quitanta da Marcia", "Rua x", "Josefina", "9999999"),
+            Cliente("Quitanta da Maria", "Rua y", "Maria", "8888888"),
+            Cliente("Mercado da esquina", "Rua z", "José", "777777"),
+            Cliente("Padaria", "Rua w", "Marcos", "666666"),
+            Cliente("Sacolão do Marcello", "Rua a", "Cara simpática", "5555555")
+        )
+
+        listaClientes.addAll(mutableListOf)
+        return listaClientes
+    }
+
+    private fun configuraToolbar() {
+        val toolbarid = toolbarid2
+        setSupportActionBar(toolbarid)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        colappsingtoolbar.title = "Clientes"
+    }
+
     override fun onSupportNavigateUp(): Boolean {
         onBackPressed()
         return true
+    }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+    }
+
+    override fun onContextItemSelected(item: MenuItem): Boolean {
+        var alertDialog = AlertDialog.Builder(this)
+        alertDialog.setTitle("Remover")
+        alertDialog.setMessage("Deseja remover esta conta ?")
+        alertDialog.setPositiveButton("Sim") { _, _ ->
+            Toast.makeText(this, "Sim", Toast.LENGTH_LONG).show()
+        }
+        alertDialog.setNegativeButton("Não") { _, _ ->
+            Toast.makeText(this, "Não", Toast.LENGTH_LONG).show()
+        }
+        alertDialog.show()
+        return false
     }
 }
--- app/src/main/java/com/fatec/banannap2/ui/adapter/ListClientesAdapter.kt
+++ app/src/main/java/com/fatec/banannap2/ui/adapter/ListClientesAdapter.kt
@@ -1,6 +1,7 @@
 package com.fatec.banannap2.ui.adapter
 
 import android.content.Context
+import android.view.ContextMenu
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,9 +12,9 @@ import kotlinx.android.synthetic.main.item_cliente.view.*
 
 class ListClientesAdapter(private val listaClientes: MutableList<Cliente>,
                           private val context: Context,
-                          val onClick : (Cliente) -> Unit) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+                          val onClick : (Cliente) -> Unit) : RecyclerView.Adapter<ListClientesAdapter.ViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.item_cliente, parent, false)
         return ViewHolder(view)
     }
@@ -22,18 +23,24 @@ class ListClientesAdapter(private val listaClientes: MutableList<Cliente>,
         return listaClientes.size
     }
 
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val cliente = listaClientes[position]
         holder.bindView(cliente, onClick)
     }
 
-    class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView)
+    inner class ViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView), View.OnCreateContextMenuListener {
+        override fun onCreateContextMenu(
+            menu: ContextMenu?,
+            v: View?,
+            menuInfo: ContextMenu.ContextMenuInfo?
+        ) {}
 
-    fun RecyclerView.ViewHolder.bindView(cliente: Cliente, onClick: (Cliente) -> Unit) {
-        val nomeCliente = itemView.lista_cliente_nome
-        nomeCliente.text = cliente.nomeComercio
-        itemView.setOnClickListener{ onClick(cliente) }
+        fun bindView(cliente: Cliente, onClick: (Cliente) -> Unit) {
+            val nomeCliente = itemView.lista_cliente_nome
+            nomeCliente.text = cliente.nomeComercio
+            itemView.setOnClickListener{ onClick(cliente) }
+            itemView.setOnCreateContextMenuListener(this)
 
+        }
     }
-
 }
